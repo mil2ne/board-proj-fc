ea6b825ea76115a6eebc33bf4434d521947c0182
celee <show3070@gmail.com>
2023-06-01 21:50:52 +0900

    #16-re
    user 관련 추가작업

---
+++ src/main/java/com/fastcampus/boardprojfc/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.fastcampus.boardprojfc.repository;
+
+import com.fastcampus.boardprojfc.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
