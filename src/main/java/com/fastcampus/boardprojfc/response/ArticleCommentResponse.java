40dd05c6e429762d6f126b64134c31e69b7dd0b2
celee <show3070@gmail.com>
2023-06-05 23:03:33 +0900

    #18 - 서비스 로직 dto 재설계

---
+++ src/main/java/com/fastcampus/boardprojfc/response/ArticleCommentResponse.java
@@ -0,0 +1,36 @@
+package com.fastcampus.boardprojfc.response;
+
+import com.fastcampus.boardprojfc.dto.ArticleCommentDto;
+
+import java.time.LocalDateTime;
+
+public record ArticleCommentResponse(
+        Long id,
+        String content,
+        LocalDateTime createdAt,
+        String email,
+        String nickname
+) {
+    public static ArticleCommentResponse of(Long id,
+                                            String content,
+                                            LocalDateTime createdAt,
+                                            String email,
+                                            String nickname) {
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname);
+    }
+
+    public static ArticleCommentResponse from (ArticleCommentDto dto) {
+        String nickname = dto.userAccountDto().nickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.userAccountDto().userId();
+        }
+
+        return new ArticleCommentResponse(
+                dto.id(),
+                dto.content(),
+                dto.createdAt(),
+                dto.userAccountDto().email(),
+                nickname
+        );
+    }
+}
--- src/main/java/com/fastcampus/boardprojfc/service/ArticleService.java
+++ src/main/java/com/fastcampus/boardprojfc/service/ArticleService.java
@@ -2,12 +2,14 @@ package com.fastcampus.boardprojfc.service;
 
 import com.fastcampus.boardprojfc.domain.type.SearchType;
 import com.fastcampus.boardprojfc.dto.ArticleDto;
-import com.fastcampus.boardprojfc.dto.ArticleUpdateDto;
+import com.fastcampus.boardprojfc.dto.ArticleWithCommentsDto;
 import com.fastcampus.boardprojfc.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @RequiredArgsConstructor
 @Transactional
@@ -17,21 +19,22 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_keywords) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long id) {
+    public ArticleWithCommentsDto getArticle(Long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
     }
+
 }
