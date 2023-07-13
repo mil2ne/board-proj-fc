48075fc7d8899154eaff0073a143768f47bd0c73
celee <show3070@gmail.com>
2023-07-14 07:31:24 +0900

    #54 - 대댓글 비지니스 로직 구현

--- src/main/java/com/fastcampus/boardprojfc/dto/request/ArticleCommentRequest.java
+++ src/main/java/com/fastcampus/boardprojfc/dto/request/ArticleCommentRequest.java
@@ -3,16 +3,25 @@ package com.fastcampus.boardprojfc.dto.request;
 import com.fastcampus.boardprojfc.dto.ArticleCommentDto;
 import com.fastcampus.boardprojfc.dto.UserAccountDto;
 
-public record ArticleCommentRequest(Long articleId, String content) {
+public record ArticleCommentRequest(
+        Long articleId,
+        Long parentCommentId,
+        String content
+) {
 
     public static ArticleCommentRequest of(Long articleId, String content) {
-        return new ArticleCommentRequest(articleId, content);
+        return ArticleCommentRequest.of(articleId, null, content);
+    }
+
+    public static ArticleCommentRequest of(Long articleId, Long parentCommentId, String content) {
+        return new ArticleCommentRequest(articleId, parentCommentId, content);
     }
 
     public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
         return ArticleCommentDto.of(
                 articleId,
                 userAccountDto,
+                parentCommentId,
                 content
         );
     }
